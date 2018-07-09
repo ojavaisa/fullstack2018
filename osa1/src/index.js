4b9ed4112d097fa8c04c358a119320ab8edd12f9
ojavaisa <olli.vaisanen@iki.fi>
2018-07-09 14:24:57 +0300

    Osio 1 alku

---
+++ osa1/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    console.log('Hello from komponentti')
+    return (
+    <div>
+        <p>Hello world</p>
+    </div>
+    )}
+
+ReactDOM.render(<App />, document.getElementById('root'))
